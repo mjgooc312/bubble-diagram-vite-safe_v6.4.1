8d78738ef63e51fbff36a80d90fdbac0fd04976f
Mark Jay Gooc <50866978+mjgooc312@users.noreply.github.com>
2025-11-15 08:03:46 +0800

    Update BubbleAdjacencyApp.jsx

--- src/BubbleAdjacencyApp.jsx
+++ src/BubbleAdjacencyApp.jsx
@@ -2875,7 +2875,39 @@ export default function BubbleAdjacencyApp() {
                           {n.area} m²
                         </text>
                       )}
+
+                      {/* inline editors (disabled in connect mode) */}
+                      <foreignObject
+                        x={-r}
+                        y={-18}
+                        width={r * 2}
+                        height={36}
+                        data-ignore-export
+                        style={{ pointerEvents: mode === "connect" ? "none" : "auto" }}
+                      >
+                        <InlineEdit
+                          text={n.name}
+                          onChange={(val) => renameNode(n.id, val)}
+                          className="mx-auto text-center"
+                        />
+                      </foreignObject>
+                      <foreignObject
+                        x={-40}
+                        y={r - 22}
+                        width={80}
+                        height={26}
+                        data-ignore-export
+                        style={{ pointerEvents: mode === "connect" ? "none" : "auto" }}
+                      >
+                        <InlineEdit
+                          text={`${n.area}`}
+                          onChange={(val) => changeArea(n.id, val)}
+                          className="text-center"
+                        />
+                      </foreignObject>
                     </g>
+                  );
+                })}
 
                 {/* LINKS (above bubbles) */}
                 {links.map((l) => {
